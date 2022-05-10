8d826d6112536b071782ffaaf30691a8776edf46
Damian <tricasaurio@gmail.com>
2022-05-10 17:10:20 +0200

    Lesson 1 Exercise 1./2./3. DONE

---
+++ src/components/Heading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Logo from "../139899.svg";
+
+export function Heading() {
+  return (
+    <header className="App-header">
+      <img src={Logo} alt="Hotel logo" />
+      CYF Hotel
+    </header>
+  );
+}
---
+++ src/components/TouristInfoCards.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export function TouristInfoCards() {
+  return (
+    <div>
+      <div className="card">
+        <img src="" alt="Glasgow" className="card-img-top" />
+        <div className="card-body" />
+      </div>
+      <div>
+        <div className="card">
+          <img src="" alt="Manchester" className="card-img-top" />
+          <div className="card-body" />
+        </div>
+      </div>
+      <div>
+        <div className="card">
+          <img src="" alt="London" className="card-img-top" />
+          <div className="card-body" />
+        </div>
+      </div>
+    </div>
+  );
+}
